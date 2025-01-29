6e9f18e7112da18388d39a6ec4c9ba65598b81ae
ra1nfall~ <georg555ru@gmail.com>
2025-01-29 20:57:43 +0300

    ver 3

--- src/widget.py
+++ src/widget.py
@@ -1,23 +1,30 @@
-from masks import get_mask_card_number, get_mask_account
+from masks import get_mask_account, get_mask_card_number
+
+
 def mask_account_card(card: str) -> str:
     words = card.split()
     counter = 1
     for i in words:
-        if i[0] in '0123456789':
+        if i[0] in "0123456789":
             number = i
             break
         counter += 1
     counter_two = 0
-    type_card = ''
+    type_card = ""
     while counter_two < counter - 1:
         if counter_two != 0:
-            type_card += ' '
+            type_card += " "
         type_card += words[counter_two]
         counter_two += 1
-    if type_card == 'Счет':
-        result = f'Счет {get_mask_account(int(number))}'
+    if type_card == "Счет":
+        result = f"Счет {get_mask_account(int(number))}"
         return result
     else:
-        result = f'{type_card} {get_mask_card_number(int(number))}'
+        result = f"{type_card} {get_mask_card_number(int(number))}"
         return result
-print(mask_account_card('Visa Platinum 7000792289606361'))
+
+
+def get_date(time: str) -> str:
+    time_list = time.split("-")
+    result = f"{time_list[2][:2]}:{time_list[1]}:{time_list[0]}"
+    return result
