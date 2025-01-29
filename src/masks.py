e899a6847b7cfb388a45dc7aefcc53166c9fae65
ra1nfall~ <georg555ru@gmail.com>
2025-01-29 20:21:30 +0300

    ver 1

---
+++ src/masks.py
@@ -0,0 +1,20 @@
+def get_mask_card_number(number: int) -> str:
+    result = ""
+    counter = 1
+    for i in str(number):
+        if counter == 4:
+            result += i
+            result += " "
+        elif counter == 8 or counter == 12:
+            result += "* "
+        elif counter > 6 and counter <= 12:
+            result += "*"
+        else:
+            result += i
+        counter += 1
+    return result
+
+
+def get_mask_account(number: int) -> str:
+    result = "**" + str(number)[-4::]
+    return result
