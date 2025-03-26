4bdd4a4063fb41ace7a8a0bb7b3956a9e2cb9e65
ra1nfall~ <georg555ru@gmail.com>
2025-03-26 13:37:25 +0300

    hw_generators done 2.0

--- src/generators.py
+++ src/generators.py
@@ -1,14 +1,17 @@
 def filter_by_currency(transactions: list, value: str) -> dict:
+    """Функция должна возвращать итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной (например, USD)."""
     for i in transactions:
         if i['operationAmount']['currency']['code'] == value:
             yield i
 
 
 def transaction_descriptions(transactions: list) -> str:
+    """генератор, который принимает список словарей с транзакциями и возвращает описание каждой операции по очереди."""
     for i in transactions:
         yield i['description']
 
 def card_number_generator(start: int, end: int) -> str:
+    """генератор, который выдает номера банковских карт в формате XXXX XXXX XXXX XXXX, где X — цифра номера карты. Генератор может сгенерировать номера карт в заданном диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999."""
     for i in range(start, end + 1):
         fours = (16 - len(str(i))) // 4
         zeros = 4 - (len(str(i)) % 4)
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,15 +1,21 @@
-from masks import get_mask_card_number, get_mask_account
+
+from src.masks import get_mask_card_number, get_mask_account
 import pytest
+
+
 @pytest.fixture
 def fixture_card_number():
     return '7000792289606361'
 
+
 def test_get_mask_card_number(fixture_card_number):
     assert get_mask_card_number(fixture_card_number) == '7000 79** **** 6361'
+
 
 @pytest.fixture
 def fixture_mask_account():
     return '73654108430135874305'
 
-def test_get_mask_account(fixture_card_number):
+
+def test_get_mask_account(fixture_mask_account):
     assert get_mask_account(fixture_mask_account) == '**4305'
